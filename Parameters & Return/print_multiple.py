f879cfd2b4fe2bbdf098f23111a6b8639dea14a3
Solon P <freeberd83@gmail.com>
2021-05-15 16:16:40 +0300

    Add files via upload

---
+++ Parameters & Return/print_multiple.py
@@ -0,0 +1,31 @@
+"""
+Print multiple
+
+Fill out print_multiple(message, repeats), which takes as parameters a string message to print, and an integer repeats number of times to print message. We've written the main() function for you, which prompts the user for a message and a number of repeats.
+
+Here's a sample run of the program:
+
+$ python print_multiple.py
+Please type a message: Hello!
+Enter a number of times to repeat your message: 6
+Hello!
+Hello!
+Hello!
+Hello!
+Hello!
+Hello!
+"""
+
+def print_multiple(message, repeats):
+    # write your code below!
+    for i in range(repeats):
+        print(message)
+    
+def main():
+    message = input("Please type a message: ")
+    repeats = int(input("Enter a number of times to repeat your message: "))
+    print_multiple(message, repeats)
+
+if __name__ == "__main__":
+    main()
+
